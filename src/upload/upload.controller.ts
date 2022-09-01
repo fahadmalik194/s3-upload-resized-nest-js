173c06a6c5148505325a648565294c3ba6b1aa64
fahadmalik194 <muhammad.fahad@techverx.com>
2022-09-01 19:26:18 +0500

    Http Status Code Updated

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { UploadService } from './upload.service'
 import { UploadDto } from './upload.dto'
@@ -7,6 +7,7 @@ import { UploadDto } from './upload.dto'
 export class UploadController {
     constructor(private uploadService: UploadService) {}
 
+    @HttpCode(HttpStatus.CREATED)
     @Post()
     @UseInterceptors(FileInterceptor('fileToUpload'))
     uploadToS3(@Body() bodyObj: UploadDto, @UploadedFile() fileObj: Express.Multer.File) {
